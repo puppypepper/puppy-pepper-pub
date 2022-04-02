45a40b2b27fa3f9b775db89d48b1e8917f2a2893
p6 <puppypepper21@gmail.com>
2022-04-03 04:02:47 +0900

    update for unity with Next.js

--- app/frontend/p6-front/pages/index.tsx
+++ app/frontend/p6-front/pages/index.tsx
@@ -1,11 +1,25 @@
 import type { NextPage } from 'next'
+import Unity, { UnityContext } from "react-unity-webgl"
+
 import Layout from 'components/Layout'
+
 import styles from 'styles/Home.module.css'
+
+const unityContext = new UnityContext({
+  loaderUrl: "Breakout/Build/Breakout.loader.js",
+  dataUrl: "Breakout/Build/Breakout.data.gz",
+  frameworkUrl: "Breakout/Build/Breakout.framework.js.gz",
+  codeUrl: "Breakout/Build/Breakout.wasm.gz",
+})
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Layout>
+      <Layout title="p6">
+        <Unity 
+          unityContext={ unityContext }
+          className={styles.unityCanvas}
+        />
       </Layout>
     </div>
   )
